2f8f1ee92a9a4532d872cefc5449c12c77712cef
Champ-Goblem <cameron@northflank.com>
2026-03-23 11:41:35 +0000

    update the dd command

--- fuse-demo.sh
+++ fuse-demo.sh
@@ -6,6 +6,6 @@ echo "--- Mounting GCS ---"
 time gcsfuse --enable-hns=false --implicit-dirs --key-file /auth $GCS_FUSE_BUCKET /gcs
 echo "--- GCS Mounted ---"
 
-dd if=/dev/zero of=/gcs/file-write bs=512 count=2097152 status=progress
+dd if=/dev/zero of=/gcs/file-write bs=512 count=2024 status=progress
 
 sleep infinity
